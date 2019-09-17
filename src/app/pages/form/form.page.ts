cbdf246d02dbbfa8f22de776967c94a8fc5b64c8
kimniyom <kimniyomclub@gmail.com>
2019-09-17 16:33:35 +0700

    update form

--- src/app/pages/form/form.page.ts
+++ src/app/pages/form/form.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Platform, NavController } from 'ionic-angular';
@@ -14,11 +14,14 @@ export class FormPage implements OnInit {
   lname;
   sex;
   privilege;
-
-
+  age;
+  waistline;
+  showTrue: boolean = false;
   constructor(
     private router: Router,
-    private alert: AlertController
+    private alert: AlertController,
+    private http: Http,
+    @Inject('API_URL') private API_URL: string,
   ) { }
 
   ngOnInit() {
@@ -36,24 +39,56 @@ export class FormPage implements OnInit {
     await alert.present();
   }
 
-  save() {
+  checkCid(){
     let cid = this.cid;
-    if (!this.sex || !this.cid || !this.name || !this.lname || !this.privilege) {
+    if (!this.cid || !this.privilege) {
       //alert("กรอกข้อมูลไม่ครบ...");
       this.Alert("กรอกข้อมูลไม่ครบ...");
       return false;
     }
+
     if(cid.length != 13){
       this.Alert("เลขบัตรประชาชนต้อง 13 หลัก...");
       return false;
     }
 
+    this.http.get(this.API_URL + "/person/" + cid).subscribe(res => {
+      let data =  res.json();
+      let person = data.rows[0][0];
+      console.log(person);
+      if(!person){
+        this.showTrue = false;
+        this.Alert("ไม่พบข้อมูลในระบบจังหวัด...");
+        return false;
+      } else {
+        this.showTrue = true;
+        this.name = person.NAME;
+        this.lname = person.LNAME;
+        if(person.SEX == 1){
+          this.sex = "M";
+        } else {
+          this.sex = "F";
+        }
+      }
+      this.age = person.AGE;
+    })
+  }
+
+  save() {
+    if (!this.cid || !this.privilege || !this.name || !this.lname || !this.age || !this.sex || !this.waistline) {
+      this.Alert("กรอกข้อมูลไม่ครบ...");
+      return false;
+    }
+    
+    
     let data = {
       cid: this.cid,
       sex: this.sex,
       name: this.name,
       lname: this.lname,
-      privilege: this.privilege
+      age: this.age,
+      privilege: this.privilege,
+      waistline: this.waistline
     }
     //console.log(data);
     sessionStorage.setItem("form", JSON.stringify(data));
