499a2150337fb44749beba1f85b12e355cdd5808
kimniyom <kimniyomclub@gmail.com>
2019-09-30 15:42:19 +0700

    modal popup formular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormuladmPage } from './pages/formuladm/formuladm.page';
-import { FormulahtPage } from './pages/formulaht/formulaht.page';
+import { FormularhtPage } from './pages/formularht/formularht.page';
 import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormuladmPage,
-    FormulahtPage
+    FormularhtPage
   ],
   entryComponents: [
     FormuladmPage
