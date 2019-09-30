dce344337e91626cb41faf907878b133f1e283ca
kimniyom <kimniyomclub@gmail.com>
2019-09-30 16:34:49 +0700

    modal popup pages formular ht

--- src/app/pages/formularht/formularht.page.ts
+++ src/app/pages/formularht/formularht.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-formularht',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularhtPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController,
+  ) { }
 
   ngOnInit() {
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      'dismissed': true
+    });
   }
 
 }
